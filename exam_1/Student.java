ecc786c697e438041aeb844f804c1ce2af58bc1a
Gabriel Catapang <111548759+Sephiroth53@users.noreply.github.com>
2022-09-06 09:37:12 +0800

    Create Student.java

---
+++ exam_1/Student.java
@@ -0,0 +1,33 @@
+public class Student {
+
+    String surname;
+    String firstName;
+    char middleInitial;
+    String dateOfBirth;
+    int studentNumber;
+    String studentemailaddress;
+    Boolean IamAwesome; 
+
+    // Method
+    public void sayMysurname() {
+        System.out.println("surname: " + surname);
+    }
+        public void sayMyfirstName() {
+            System.out.println("firstname: " + firstName);
+    }
+    public void sayMymiddleInitial() {
+        System.out.println("middleInitial: " + middleInitial);
+    }
+    public void sayMydateofBirth() {
+        System.out.println("dateOfBirth: " + dateOfBirth);
+    }
+    public void sayMystudentNumber() {
+        System.out.println("studentNumber: " + studentNumber);
+    }
+    public void sayMystudentemailaddress() {
+        System.out.println("studentEmailAddress: " + studentemailaddress);
+    }
+    public void sayIamAwesome() {
+        System.out.println("iAmAwesome: " + IamAwesome);
+    }
+} 
